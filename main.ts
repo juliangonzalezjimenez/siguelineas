fde6f4d05194a41735645552150e42e0389e8599
juliangonzalezjimenez <98742865+juliangonzalezjimenez@users.noreply.github.com>
2022-03-23 09:19:52 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,15 @@
 basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
     } else {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
     }
     if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
     } else {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
     }
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
-    } else {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
     }
 })
